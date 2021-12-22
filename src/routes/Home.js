85cae37bf1c5249e5818f5c71147850b32856bb0
reza khani <reza.khani1998@gmail.com>
2021-12-22 19:13:58 +0330

    Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,32 @@
-const Home = () => {
-	return <div>
-		<form>
-			
-		</form>
+import { useState } from 'react';
 
-	</div>;
+const Home = () => {
+	const [rweet, setRweet] = useState('');
+
+	const onSubmitHandler = (event) => {
+		event.preventDefault();
+	};
+
+	const onChangeHandler = (event) => {
+		const enteredInput = event.target.value;
+		setRweet(enteredInput);
+		console.log(rweet);
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmitHandler}>
+				<input
+					type="text"
+					placeholder="what's in your mind?"
+					onChange={onChangeHandler}
+					value={rweet}
+					maxLength={120}
+				></input>
+				<button type="submit">Rweet</button>
+			</form>
+		</div>
+	);
 };
 
 export default Home;
